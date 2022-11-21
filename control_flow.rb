2976923ac82db41d8ec0b7a6ba9285ba92c97b31
Ceri Dorsey-Tyler <cerimay@gmail.com>
2022-11-21 16:46:08 -0500

    commit tree fd1db5e527560e920ee1ebb18bd280faf7f22276

--- control_flow.rb
+++ control_flow.rb
@@ -1,13 +1,31 @@
 def admin_login(username, password)
-  # your code here
+  if (username == "admin" || username == "ADMIN") && password == "12345"
+    "Access granted"
+  else 
+    "Access denied"
+  end
 end
 
 def hows_the_weather(temperature)
-  # your code here
+  if temperature <40 
+    "It's brisk out there!"
+  elsif temperature >= 40 and temperature <= 60
+    "It's a little chilly out there!"
+  elsif temperature > 85
+    "It's too dang hot out there!"
+  else 
+    "It's perfect out there!"
+  end
 end
 
 def fizzbuzz(num)
-  # your code here
+  if num % 3 == 0
+    puts "Fizz"
+  elsif num % 5 == 0 
+    puts "Buzz"
+  else
+    "FizzBuzz"
+  end
 end
 
 def calculator(operation, num1, num2)
